54b458df09fff31aa4548cd8f094cc7c81f5b97a
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-07-02 13:22:31 +0200

    feat: implement initial drag and drop support in ProjectItem

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
+// Drag & Drop Interface
+interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project type
-enum ProjectStatus {Active, Finished}
+enum ProjectStatus {
+    Active,
+    Finished
+}
 
 class Project {
     constructor(
@@ -150,7 +165,8 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem Class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable {
     private project: Project;
 
     get persons() {
@@ -169,7 +185,18 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.renderContent();
     }
 
-    configure() {}
+    @autobind
+    dragStartHandler(event: DragEvent) {
+        console.log(event);
+    }
+    dragEndHandler(_: DragEvent) {
+        console.log("Dragend");
+    }
+
+    configure() {
+        this.element.addEventListener("dragstart", this.dragStartHandler);
+        this.element.addEventListener("dragend", this.dragEndHandler);
+    }
 
     renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title;
